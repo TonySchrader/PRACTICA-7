1fdbabd9d51cdffa7b8cc63d320d18b193865b9c
TonySchrader <schraderantonio@gmail.com>
2025-10-09 14:19:43 -0600

    Programa1.c

--- Programa1.c
+++ Programa1.c
@@ -1,13 +1,13 @@
-ed#include<stdio.h>
+#include<stdio.h>
 int main ()
 {
-int a, b;
-a = 3;
-b = 2;
-if (a > b)
-{
-printf("\ta (%d) es mayor a b (%d).\n",a,b);
-}
-printf("\tEl programa sigue su flujo.\n");
-return 0;
+  int a, b;
+  a = 3;
+  b = 2;
+  if (a > b)
+  {
+  printf("\ta (%d) es mayor a b (%d).\n",a,b);
+  }
+  printf("\tEl programa sigue su flujo.\n");
+  return 0;
 }
